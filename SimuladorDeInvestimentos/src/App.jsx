b67329a96f6382736e87dd20b80bb2010ec1b604
Gabriel Souza <gbr_souza06@outlook.com>
2026-04-10 21:40:17 -0300

    build(p1): configura o bootstrap e reponsividade

---
+++ SimuladorDeInvestimentos/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8" style={{
+            fontSize: 30
+          }}>Hello, Investimento</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
