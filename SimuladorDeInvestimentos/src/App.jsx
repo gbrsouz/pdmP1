77654c7abd9c97ead5ee91bcb1088a9df31a702c
Gabriel Souza <gbr_souza06@outlook.com>
2026-04-16 20:01:24 -0300

    refactor(p1): atualiza a estrutura e design do app

--- SimuladorDeInvestimentos/src/App.jsx
+++ SimuladorDeInvestimentos/src/App.jsx
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <div class="container">
-      <div class="row">
+      <div class="row justify-content-center">
         <div class="col-md-8 mt-5" style={{
             fontSize: 30
           }}>
-            <h1>Simulador de Investimentos</h1>
-            <h4>Descubra quanto seu dinheiro pode render com juros compostos</h4>
+          <h1>Simulador de Investimentos</h1>
+          <h4>Descubra quanto seu dinheiro pode render com juros compostos</h4>
           <CapturaDados
             setValorFinal = {setValorFinal}
             setTotal = {setTotal}
@@ -27,29 +27,43 @@ function App() {
             historico = {historico}
             setHistorico = {setHistorico}
           ></CapturaDados>
-          <ExibeDados
-            valorFinal = {valorFinal}
-            total = {total}
-            aporte = {aporte}
-            juros = {juros}
-            rentabilidade = {rentabilidade}
-          ></ExibeDados>
-          <table className="table mt-4" style={{
-            border: "2px solid gray"
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-8 mt-4" style={{
+            fontSize: 30
+          }}>
+            <ExibeDados
+              valorFinal = {valorFinal}
+              total = {total}
+              aporte = {aporte}
+              juros = {juros}
+              rentabilidade = {rentabilidade}
+            ></ExibeDados>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="col-md-6 mt-5"><h4>Histórico de Simulações</h4></div>
+          <div class="col-md-6"><h6>{historico.push()} Simulações</h6></div>
+          <table className="table" style={{
+            border: "2px solid gray",
+            borderRadius: "10px",
+            borderCollapse: "separate"
           }}>
               <thead>
                 <tr style={{fontSize:20}}>
-                  <th>Data</th>
-                  <th>Valor (R$)</th>
+                  <th style={{ border: "none" }}>Data</th>
+                  <th style={{ border: "none" }}>Valor (R$)</th>
                 </tr>
               </thead>
               <tbody>
                 {historico.map((registro, i) => (
                   <tr key={i} style={{fontSize:15}}>
-                    <td>
+                    <td style={{ border: "none" }}>
                       {new Date(registro.data).toLocaleString()}
                     </td>
-                    <td>
+                    <td style={{ border: "none" }}>
                       {registro.valor}
                     </td>
                   </tr>
